032a52eec67b9fea7204f3d06aa6fdb82b0ffc90
juan52878911 <juanbedoya843@gmail.com>
2023-05-05 00:10:31 -0500

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -15,47 +15,86 @@ const heart = [
 ];
 
 let time;
+let drawingContext;
+let canvas_text;
+
+let scale;
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight, WEBGL);
     time = 0;
+
+    // Texto de feliz cumpleaños
+    canvas_text = createGraphics(window.innerWidth, window.innerHeight);
+    canvas_text.textFont('Source Code Pro');
+    canvas_text.textAlign(CENTER);
+    canvas_text.textSize(32);
+    canvas_text.background(255, 0);
+    canvas_text.fill(0);
+    canvas_text.noStroke();
+    canvas_text.text("Eres la mejor persona \nque alguna vez pude conocer", width * 0.5, 50);
+    canvas_text.text("Expandamos nuestro pequeño infinito\nmas alla de la realidad ", width * 0.5, height-50);
+
+    scale = (width<height ? width/20 : height/30);
 }
 
 function setup() {
     createCanvas(windowWidth, windowHeight, WEBGL);
+    drawingContext = canvas.getContext('webgl');
     time = 0;
+
+    // Texto de feliz cumpleaños
+    canvas_text = createGraphics(window.innerWidth, window.innerHeight);
+    canvas_text.textFont('Source Code Pro');
+    canvas_text.textAlign(CENTER);
+    canvas_text.textSize(32);
+    canvas_text.background(255, 0);
+    canvas_text.fill(0);
+    canvas_text.noStroke();
+    canvas_text.text("Eres la mejor persona \nque alguna vez pude conocer", width * 0.5, 50);
+    canvas_text.text("Expandamos nuestro pequeño infinito\nmas alla de la realidad ", width * 0.5, height-50);
+
+    scale = (width<height ? width/20 : height/30);
 }
 
 function draw() {
-    background(0);
+    background(255, 0);
     orbitControl();
 
-    ambientLight(100, 100, 100);
-    directionalLight(10, 20, 20, 1, 0.5, -1);
-    directionalLight(70, 70, 100, -0.5, 1, 0.5)
+    translate(0,0,-200)
+    texture(canvas_text);
+    plane(window.innerWidth - 4, window.innerHeight - 4);
+
+    translate(0,0,200)
+    directionalLight(255, 255, 255, 1, 100, 100, -100)
+    directionalLight(255, 255, 255, 1, -100, 100, 100)
+    directionalLight(255, 255, 255, 1, 100, -100, 100)
+    directionalLight(255, 255, 255, 1, 0, 0, -100)
 
     // centramos el corazón
-    translate(-heart[0].length * 25 / 2, -heart.length * 25 / 2);
-
-    let scale = 25;
+    
+    let gap = 0;
     noStroke();
     fill(255)
+    
+    rotateY(time * 0.9);
+    translate(-heart[0].length * (scale / 2) + gap, -heart.length * (scale / 2));
 
     for (let y = 0; y < heart.length; y++) {
         for (let x = 0; x < heart[0].length; x++) {
-            translate(scale+(Math.sin(x)), 0);
+            translate(scale, 0);
 
             switch (heart[y][x]) {
                 case 0:
                     continue;
                 case 1:
-                    specularMaterial(0)
+                    fill(0);
                     break;
                 case 2:
-                    specularMaterial(255, 0, 0);
+                    fill(255, 0, 0);
                     break;
                 case 3:
-                    specularMaterial(255);
+                    fill(255);
                     break;
             }
 
@@ -63,7 +102,7 @@ function draw() {
             box(scale, scale, scale);
             pop();
         }
-        translate(-heart[0].length * scale, scale+(2*Math.cos(time)));
+        translate(-heart[0].length * scale, scale);
     }
 
     time += 0.01;
